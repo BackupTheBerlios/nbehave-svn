e75362035b042ee3da37f7b633494f6440440fb9
lazydev <lazydev@a54d4db6-692a-0410-b4f1-ec3c4ddf56ab>
2007-07-25 22:10:30 +0000

    another file i missed.

---
+++ trunk/NBehave.Framework.Behaviour/FullTextRunnerBehaviour.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NMock2;
+using NBehave.Framework.Story;
+
+
+namespace NBehave.Framework.Behaviour
+{
+    [TestFixture]
+    public class FullTextRunnerBehaviour
+    {
+        [Test]
+        public void ShouldRunStoryAndGetStuffInStream()
+        {
+            //Given
+            string expected = "Story: Fake story.  --> Passed" + Environment.NewLine;
+
+            FakeStory story = new FakeStory(); // mocks.NewMock<IStory<SimplestPossibleWorld>>();
+
+            System.Collections.ArrayList stories = new System.Collections.ArrayList();
+            stories.Add(story);
+            Stream sr = new MemoryStream();
+            StreamRunner storyRunner = new FullTextRunner(sr, stories);
+
+            //When
+            storyRunner.Run();
+
+            //Then
+            sr.Seek(0, 0);
+            TextReader s = new StreamReader(sr);
+            Assert.AreEqual(expected, s.ReadToEnd());
+        }
+
+    }
+}
