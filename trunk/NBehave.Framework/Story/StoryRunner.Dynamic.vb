93f379aa1fedd18aead4977d2113d8ec209e24d7
lazydev <lazydev@a54d4db6-692a-0410-b4f1-ec3c4ddf56ab>
2007-08-08 11:43:20 +0000

    missed a file, again...

---
+++ trunk/NBehave.Framework/Story/StoryRunner.Dynamic.vb
@@ -0,0 +1,15 @@
+Option Strict Off
+
+
+Namespace Story
+
+    Partial Public Class StoryRunner
+
+        Private Sub AddScenarios(ByRef aStory As Object)
+            aStory.Scenarios()
+        End Sub
+
+    End Class
+
+
+End Namespace
